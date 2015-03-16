702db8333326a46c146fa303775a5afbd9a2ffd7
Franck Chauvel <franck.chauvel@gmail.com>
2015-03-16 15:48:19 +0100

    Adding a performance module

---
+++ performance/src/main/java/eu/diversify/trio/performance/Runner.java
@@ -0,0 +1,13 @@
+
+package eu.diversify.trio.performance;
+
+/**
+ * Entry point of the performance test
+ */
+public class Runner {
+
+    public static void main(String[] args) {
+        System.out.println("This is a performance test");
+    }
+    
+}
