c36ecc820c09b66b98bc4e6680adb7f9b4534a1d
Franck Chauvel <franck.chauvel@gmail.com>
2015-04-07 15:16:51 +0200

    Adding calculation of the 'average node degree'

--- core/src/main/java/eu/diversify/trio/core/statistics/Density.java
+++ core/src/main/java/eu/diversify/trio/core/statistics/Density.java
@@ -15,13 +15,27 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with TRIO.  If not, see <http://www.gnu.org/licenses/>.
  */
+/**
+ *
+ * This file is part of TRIO.
+ *
+ * TRIO is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * TRIO is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TRIO. If not, see <http://www.gnu.org/licenses/>.
+ */
 package eu.diversify.trio.core.statistics;
 
-import eu.diversify.trio.core.Component;
-import eu.diversify.trio.core.DefaultAssemblyVisitor;
 import eu.diversify.trio.core.requirements.stats.VariableCount;
 import eu.diversify.trio.core.Dispatcher;
-import eu.diversify.trio.util.Require;
 
 /**
  * Compute the density of the graph underlying the structure of system.
@@ -31,50 +45,19 @@ import eu.diversify.trio.util.Require;
  */
 public class Density extends Dispatcher {
 
-    private final DensityCalculator calculator;
+    private final GraphSizeCalculator calculator;
 
     public Density() {
-        this(new DensityCalculator(new VariableCount()));
+        this(new GraphSizeCalculator(new VariableCount()));
     }
 
-    private Density(DensityCalculator calculator) {
+    private Density(GraphSizeCalculator calculator) {
         super(calculator.getVariableCounter(), calculator);
         this.calculator = calculator;
     }
 
     public double getValue() {
-        return calculator.getValue();
-    }
-
-    private static class DensityCalculator extends DefaultAssemblyVisitor {
-
-        private final VariableCount variableCount;
-        private int componentCount;
-        private int edgeCount;
-
-        public DensityCalculator(VariableCount variableCount) {
-            Require.notNull(variableCount, "Invalid value 'null' given as variable counter");
-
-            this.variableCount = variableCount;
-        }
-
-        @Override
-        public void exit(Component component) {
-            componentCount++;
-            edgeCount += variableCount.get();
-        }
-
-        public VariableCount getVariableCounter() {
-            return this.variableCount;
-        }
-
-        public double getValue() {
-            assert componentCount != 0:
-                    "No component where traversed yet!";
-
-            return ((double) edgeCount) / (componentCount * componentCount);
-        }
+        return ((double) calculator.getEdgeCount()) / Math.pow(calculator.getNodeCount(), 2);
 
     }
-
 }
---
+++ core/src/test/java/eu/diversify/trio/unit/core/statistics/AverageNodeDegreeTest.java
@@ -0,0 +1,105 @@
+/**
+ *
+ * This file is part of TRIO.
+ *
+ * TRIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TRIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TRIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.diversify.trio.unit.core.statistics;
+
+import eu.diversify.trio.core.Assembly;
+import eu.diversify.trio.core.Component;
+import static eu.diversify.trio.core.Evaluation.evaluate;
+import static eu.diversify.trio.core.requirements.Factory.require;
+import eu.diversify.trio.core.statistics.AverageNodeDegree;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ * Specification of the average node degree (k-bar), commonly defined as 2*m /
+ * n, where m is the number of link and n is the number of node is a graph.
+ */
+@RunWith(Parameterized.class)
+public class AverageNodeDegreeTest {
+
+    private static final double ERROR_TOLERANCE = 1e-6;
+
+    private final String testName;
+    private final Assembly assembly;
+    private final AverageNodeDegree averageNodeDegree;
+    private final double expectedValue;
+
+    public AverageNodeDegreeTest(String testName, Assembly assembly, double expectedValue) {
+        this.testName = testName;
+        this.assembly = assembly;
+        this.averageNodeDegree = new AverageNodeDegree();
+        this.expectedValue = expectedValue;
+    }
+
+    @Test
+    public void shouldComputeAverageNodeDegreeProperly() {
+        evaluate(averageNodeDegree).on(assembly);
+        assertThat(averageNodeDegree.getValue(), is(closeTo(expectedValue, ERROR_TOLERANCE)));
+    }
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<Object[]> examples() {
+        final List<Object[]> examples = new ArrayList<Object[]>();
+
+        examples.add(new Object[]{
+            "A",
+            new Assembly(
+            new Component("A")
+            ),
+            0D
+        });
+
+        examples.add(new Object[]{
+            "A requires B",
+            new Assembly(
+            new Component("A", require("B")),
+            new Component("B")
+            ),
+            1
+        });
+
+        examples.add(new Object[]{
+            "A requires B or C",
+            new Assembly(
+            new Component("A", require("B").or(require("C"))),
+            new Component("B"),
+            new Component("C")
+            ),
+            2D * 2 / 3
+        });
+
+        examples.add(new Object[]{
+            "Ring topology (n=3)",
+            new Assembly(
+            new Component("A", require("B")),
+            new Component("B", require("C")),
+            new Component("C", require("A"))
+            ),
+            2
+        });
+
+        return examples;
+    }
+}
