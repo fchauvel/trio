1085301689251c3fb13dd7bd2f2cc3ac7d3a46f0
Franck Chauvel <franck.chauvel@gmail.com>
2015-04-16 13:52:17 +0200

    Exposing all possible parameters in the setup file of the performance benchmark

--- performance/src/main/java/eu/diversify/trio/performance/GeneratorSetupRandomizer.java
+++ performance/src/main/java/eu/diversify/trio/performance/GeneratorSetupRandomizer.java
@@ -2,6 +2,7 @@ package eu.diversify.trio.performance;
 
 import eu.diversify.trio.generator.Setup;
 import eu.diversify.trio.utility.Count;
+import eu.diversify.trio.utility.Probability;
 import eu.diversify.trio.utility.pdf.PDF;
 import eu.diversify.trio.utility.pdf.UniformDistribution;
 
@@ -12,26 +13,66 @@ import eu.diversify.trio.utility.pdf.UniformDistribution;
 public class GeneratorSetupRandomizer {
 
     public static final PDF DEFAULT_NODE_COUNT_PDF = new UniformDistribution(10, 100);
+    public static final PDF DEFAULT_EDGE_CREATION_PDF = new UniformDistribution(0, 1);
+    public static final PDF DEFAULT_NEIGHBOR_COUNT_PDF = new UniformDistribution(0, 1);
+    public static final PDF DEFAULT_EDGE_UPDATE_PDF = new UniformDistribution(0, 1);
+    public static final PDF DEFAULT_ALPHA_PDF = new UniformDistribution(0, 1);
+    public static final PDF DEFAULT_BETA_PDF = new UniformDistribution(0, 1);
 
     private final PDF nodeCount;
-    
+    private final PDF edgeCreation;
+    private final PDF neighborCount;
+    private final PDF edgeUpdate;
+    private final PDF alpha;
+    private final PDF beta;
+
     public GeneratorSetupRandomizer() {
-        this(DEFAULT_NODE_COUNT_PDF);
+        this(
+                DEFAULT_NODE_COUNT_PDF,
+                DEFAULT_EDGE_CREATION_PDF,
+                DEFAULT_NEIGHBOR_COUNT_PDF,
+                DEFAULT_EDGE_UPDATE_PDF,
+                DEFAULT_ALPHA_PDF,
+                DEFAULT_BETA_PDF);
     }
-    
-    public GeneratorSetupRandomizer(PDF nodeCount) {
+
+    public GeneratorSetupRandomizer(PDF nodeCount, PDF edgeCreation, PDF neighborCount, PDF edgeUpdate, PDF alpha, PDF beta) {
         this.nodeCount = nodeCount;
+        this.edgeCreation = edgeCreation;
+        this.neighborCount = neighborCount;
+        this.edgeUpdate = edgeUpdate;
+        this.alpha = alpha;
+        this.beta = beta;
     }
 
     public Setup next() {
+        final Count chosenNodeCount = nodeCount();
         return new Setup(
-                nodeCount(), 
-                Setup.DEFAULT_EDGE_CREATION, 
-                Setup.DEFAULT_NEIGHBOR_COUNT, 
-                Setup.DEFAULT_EDGE_UPDATE, 
-                Setup.DEFAULT_ALPHA, 
-                Setup.DEFAULT_BETA
-        ); 
+                chosenNodeCount,
+                edgeCreation(),
+                neighborCount(chosenNodeCount),
+                edgeUpdate(),
+                alpha(), beta());
+    }
+
+    private Probability beta() {
+        return new Probability(beta.sample());
+    }
+
+    private Probability alpha() {
+        return new Probability(alpha.sample());
+    }
+
+    private Probability edgeUpdate() {
+        return new Probability(edgeUpdate.sample());
+    }
+
+    private Count neighborCount(final Count chosenNodeCount) {
+        return new Count( (2 *  (int) ((chosenNodeCount.value() / 2) * neighborCount.sample())));
+    }
+
+    private Probability edgeCreation() {
+        return new Probability(edgeCreation.sample());
     }
 
     private Count nodeCount() {
--- performance/src/main/java/eu/diversify/trio/performance/setup/SetupStore.java
+++ performance/src/main/java/eu/diversify/trio/performance/setup/SetupStore.java
@@ -1,6 +1,8 @@
 package eu.diversify.trio.performance.setup;
 
 import eu.diversify.trio.performance.GeneratorSetupRandomizer;
+import static eu.diversify.trio.performance.GeneratorSetupRandomizer.*;
+import eu.diversify.trio.utility.pdf.PDF;
 import eu.diversify.trio.utility.pdf.UniformDistribution;
 import java.util.Properties;
 
@@ -21,7 +23,6 @@ public class SetupStore {
                 extractInt(properties, WARMUP_SAMPLE_COUNT_KEY, Setup.DEFAULT_WARMUP_SAMPLE_COUNT),
                 extractRandomizer(properties)
         );
-
     }
 
     private int extractInt(Properties properties, String key, int defaultValue) throws IllegalArgumentException {
@@ -40,10 +41,40 @@ public class SetupStore {
 
     private GeneratorSetupRandomizer extractRandomizer(Properties properties) {
         return new GeneratorSetupRandomizer(
-                new UniformDistribution(
-                        extractDouble(properties, ASSEMBLY_SIZE_MINIMUM_KEY, 10),
-                        extractDouble(properties, ASSEMBLY_SIZE_MAXIMUM_KEY, 100))
+                extractPDF(properties, ASSEMBLY_SIZE_KEY, DEFAULT_NODE_COUNT_PDF),
+                extractPDF(properties, EDGE_CREATION_PROBABILITY_KEY, DEFAULT_EDGE_CREATION_PDF),
+                extractPDF(properties, NEIGHBOR_COUNT_KEY, DEFAULT_NEIGHBOR_COUNT_PDF),
+                extractPDF(properties, EDGE_UPDATE_PROBABILITY_KEY, DEFAULT_EDGE_CREATION_PDF),
+                extractPDF(properties, ALPHA_KEY, DEFAULT_ALPHA_PDF),
+                extractPDF(properties, BETA_KEY, DEFAULT_BETA_PDF)
         );
+    }
+    private static final String BETA_KEY = "beta";
+    private static final String ALPHA_KEY = "alpha";
+
+    private static final String EDGE_UPDATE_PROBABILITY_KEY = "edge.update.probability";
+    private static final String NEIGHBOR_COUNT_KEY = "neighbor.count";
+    private static final String EDGE_CREATION_PROBABILITY_KEY = "edge.creation.probability";
+    private static final String ASSEMBLY_SIZE_KEY = "assembly.size";
+
+    private PDF extractPDF(Properties properties, String key, PDF defaultValue) throws IllegalArgumentException {
+        if (!properties.containsKey(maximumOf(key))
+                || !properties.containsKey(minimumOf(key))) {
+            return defaultValue;
+        }
+
+        double min = extractDouble(properties, minimumOf(key), 0);
+        double max = extractDouble(properties, maximumOf(key), 1);
+
+        return new UniformDistribution(min, max);
+    }
+
+    private String maximumOf(String key) {
+        return key + MAXIMUM_MARK;
+    }
+
+    private String minimumOf(String key) {
+        return key + MINIMUM_MARK;
     }
 
     private double extractDouble(Properties properties, String key, double defaultValue) throws IllegalArgumentException {
@@ -62,7 +93,6 @@ public class SetupStore {
 
     private static final String SAMPLE_COUNT_KEY = "sample.count";
     private static final String WARMUP_SAMPLE_COUNT_KEY = "sample.warmup.count";
-    private static final String ASSEMBLY_SIZE_MAXIMUM_KEY = "assembly.size.maximum";
-    private static final String ASSEMBLY_SIZE_MINIMUM_KEY = "assembly.size.minimum";
-
+    private static final String MINIMUM_MARK = ".minimum";
+    private static final String MAXIMUM_MARK = ".maximum";
 }
