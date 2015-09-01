3985cd87a0f4bc4be3179df2a01a4c246e8abc10
Franck Chauvel <franck.chauvel@gmail.com>
2015-09-01 10:07:37 +0200

    Updating the mycila-license plugin

--- graph/src/test/java/eu/diversify/trio/graph/storage/csv/CsvReaderTest.java
+++ graph/src/test/java/eu/diversify/trio/graph/storage/csv/CsvReaderTest.java
@@ -1,3 +1,35 @@
+/**
+ * This file is part of TRIO :: Graph.
+ *
+ * TRIO :: Graph is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TRIO :: Graph is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TRIO :: Graph.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * This file is part of TRIO.
+ *
+ * TRIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TRIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TRIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package eu.diversify.trio.graph.storage.csv;
 
 import eu.diversify.trio.graph.model.Graph;
--- performance/src/main/java/eu/diversify/trio/performance/SimulationFactory.java
+++ performance/src/main/java/eu/diversify/trio/performance/SimulationFactory.java
@@ -1,3 +1,35 @@
+/**
+ * This file is part of TRIO :: Performance.
+ *
+ * TRIO :: Performance is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TRIO :: Performance is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TRIO :: Performance.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * This file is part of TRIO.
+ *
+ * TRIO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TRIO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TRIO.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package eu.diversify.trio.performance;
 
 import eu.diversify.trio.Trio;
